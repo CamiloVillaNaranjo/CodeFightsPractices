1fa453da476d302369b796c4050eab6aabce92fa
cavilla <cavilla@nexos-software.com.co>
2017-06-01 09:03:55 -0500

    Actualizacion de Repositorio

---
+++ ArcadeFights/ArcadeFights/ExploringWaves.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArcadeFights
+{
+    public class ExploringWaves
+    {
+        protected ExploringWaves() { }
+
+        public static void Main()
+        {
+            string s = "aabb";
+            Console.WriteLine(PalindromeRearranging(s));
+            Console.ReadLine();
+        }
+
+        static bool PalindromeRearranging(string inputString)
+        {
+            bool r = false;
+
+            return r;
+        }
+    }
+}
